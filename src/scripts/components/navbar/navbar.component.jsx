12293a81102a3f9abbcbf0cc15fee27e62b22569
Sprow <ts.sprow@gmail.com>
2017-11-25 18:53:49 +0200

    small fix

---
+++ src/scripts/components/navbar/navbar.component.jsx
@@ -0,0 +1,28 @@
+// import * as React from 'react';// eslint-disable-line
+// import { Component } from 'react';
+//
+// const NavBar = React.createClass({
+//     render : function () {
+//         return (
+//             <nav className={navbar}>
+//                 <img className={navbar_logo} src={../assets/image/Gustoso.png}>
+//                     <div className={navbar_links navbar_item}>
+//                         <a href="#" className={navbar_link}>WELCOME</a>
+//                         <div className={navbar_betweenlink}>~</div>
+//                         <a href="#" className={navbar_link}>MENU</a>
+//                         <div className={navbar_betweenlink}>~</div>
+//                         <a href="#" className={navbar_link}>RESERVATIONS</a>
+//                         <div className={navbar_betweenlink}>~</div>
+//                         <a href="#" className={navbar_link}>NEWS</a>
+//                         <div className={navbar_betweenlink}>~</div>
+//                         <a href="#" className={navbar_link}>CONTACT</a>
+//                     </div>
+//                     <div className="navbar-right">
+//                         <img className={navbar-logoTwitter navbar-right_img} src={../assets/image/logo_Twitter.png}>
+//                         <img className={navbar-logoFacebook navbar-right_img} src={../assets/image/logo_Facebook.png}>
+//                         <img className={navbar-right_img} src={../assets/image/logo_Instagram.png}>
+//                     </div>
+//             </nav>
+//         )
+//     }
+// });
